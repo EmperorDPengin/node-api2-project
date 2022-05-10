f9af58f08d7094ff3024fd143089073f1c1c59cd
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-10 16:01:46 -0400

    server on

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
 // require your posts router and connect it here
+const postRouter = require('./posts/posts-router');
+
+// Routes
+server.use('/api/posts', postRouter);
+
+module.exports = server;
