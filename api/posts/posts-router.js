2291db730ce3fd698c870b06ce5bd2a7aaa29457
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-10 16:36:30 -0400

    get all comments in a post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -116,7 +116,24 @@ router.delete('/:id', (req, res) => {
 });
 
 router.get('/:id/commemts', (req, res) => {
-
+    Posts.findById(req.params.id)
+        .then(postById => {
+            if (postById) {
+                Posts.findPostComments(req.params.id)
+                    .then(foundComments => {
+                        // console.log(foundComments);
+                        res.status(200).json(foundComments);
+                    })
+                    .catch(err => {
+                        res.status(500).json({message: "The comments information could not be retrieved"});
+                    })
+            } else {
+                res.status(404).json({message: "The post with the specified ID does not exist"});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The comments information could not be retrieved" });
+        })
 });
 
 module.exports = router;
