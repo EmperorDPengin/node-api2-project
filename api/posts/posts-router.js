c81bf0e2c8b819fe55764d1b7a2aba2254754314
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-10 16:35:21 -0400

    update a post by id route

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -48,11 +48,47 @@ router.post('/', (req, res) => {
                 res.status(500).json({message: "There was an error while saving the post to the database"});
             })
     }
-
 });
 
 router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const {title, contents} = req.body;
 
+    if (title && contents && title != "" && contents != "") {
+        Posts.findById(id)
+            .then(postToUpdate => {
+                if (postToUpdate) {
+                    Posts.update(id, {title,contents})
+                        .then(updatedPosts => {
+                            if (updatedPosts > 0) {
+                                Posts.findById(id)
+                                    .then(updatedFoundByIdPost => {
+                                        if (updatedFoundByIdPost) {
+                                            res.status(200).json(updatedFoundByIdPost);
+                                        } else {
+                                            res.status(404).json({message: "The post with the specified ID does not exist"});
+                                        }
+                                    })
+                                    .catch( err => {
+                                        res.status(500).json({message: "The post information could not be modified"});
+                                    })
+                            } else {
+                                res.status(500).json({message: "The post information could not be modified"});
+                            }
+                        })
+                        .catch(err => {
+                            res.status(500).json({message: "The post information could not be modified"});
+                        })
+                } else {
+                    res.status(404).json({message: "The post with the specified ID does not exist"});
+                }
+            })
+            .catch(err => {
+                res.status(500).json({message: "The post information could not be modified"});
+            })
+    } else {
+        res.status(400).json({message: "Please provide title and contents for the post"});
+    }
 });
 
 router.delete('/:id', (req, res) => {
